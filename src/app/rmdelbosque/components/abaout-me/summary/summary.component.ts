4f72a30de11fa345e23e05da3fefb205024a0d1d
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-10-07 20:52:16 +0200

    Limpiar home y animar imagen

---
+++ src/app/rmdelbosque/components/abaout-me/summary/summary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-summary',
+  imports: [RouterLink],
+  templateUrl: './summary.component.html'
+})
+export class SummaryComponent { }
---
+++ src/app/rmdelbosque/components/portfolio/mini-porfolio/mini-porfolio.component.ts
@@ -0,0 +1,21 @@
+import { Component, computed } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { GithubService } from 'src/app/rmdelbosque/services/github.service';
+import { GithubRepoListComponent } from "../github-repo-list/github-repo-list.component";
+
+@Component({
+  selector: 'app-mini-porfolio',
+  imports: [GithubRepoListComponent, RouterLink],
+  templateUrl: './mini-porfolio.component.html'
+})
+export class MiniPorfolioComponent {
+  private githubService = new GithubService();
+  gihubRepos = this.githubService.gihubRepos;
+  gihubReposLoading = this.githubService.gihubReposLoading;
+
+  featuredProjects = computed(() =>
+    this.gihubRepos()
+      .filter((r) => !r.fork)
+      .slice(0, 3)
+  );
+}
