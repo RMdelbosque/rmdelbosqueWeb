fce319f26c4e346af3c026ac332615b031ba038e
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-10-13 18:43:33 +0200

    Quitada animación de la imagen de home y añadido degradado

--- src/app/rmdelbosque/pages/home-page/home-page.component.ts
+++ src/app/rmdelbosque/pages/home-page/home-page.component.ts
@@ -1,20 +1,24 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { MiniPorfolioComponent } from "../../components/portfolio/mini-porfolio/mini-porfolio.component";
-import { GreetingsComponent } from "../../components/abaout-me/greetings/greetings.component";
 import { SummaryComponent } from "../../components/abaout-me/summary/summary.component";
+import { GreetingsComponent } from '../../components/abaout-me/greetings/greetings.component';
 
 @Component({
   selector: 'home-page',
   imports: [MiniPorfolioComponent, GreetingsComponent, SummaryComponent],
   templateUrl: './home-page.component.html'
 })
-export default class HomePageComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    // Esperamos un momento a que el DOM y los estilos estén aplicados
-    setTimeout(() => {
-      window.scrollTo({ top: 0, behavior: 'instant' }); // 🔥 fuerza reset completo
-    }, 200); // puedes ajustar el delay (200–500 ms suele ir bien)
-  }
+// export default class HomePageComponent implements AfterViewInit {
+//   ngAfterViewInit(): void {
+//     // Esperamos un momento a que el DOM y los estilos estén aplicados
+//     setTimeout(() => {
+//       window.scrollTo({ top: 0, behavior: 'instant' }); // 🔥 fuerza reset completo
+//     }, 200); // puedes ajustar el delay (200–500 ms suele ir bien)
+//   }
 
+
+// }
+
+export default class HomePageComponent {
 
 }
