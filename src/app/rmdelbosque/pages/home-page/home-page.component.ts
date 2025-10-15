2634b4c22f4a7e88507fcf655023295939fa224f
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-10-15 21:01:05 +0200

    actualizado botones

--- src/app/rmdelbosque/pages/home-page/home-page.component.ts
+++ src/app/rmdelbosque/pages/home-page/home-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MiniPorfolioComponent } from "../../components/portfolio/mini-porfolio/mini-porfolio.component";
 import { SummaryComponent } from "../../components/abaout-me/summary/summary.component";
 import { GreetingsComponent } from '../../components/abaout-me/greetings/greetings.component';
+import { CardGameComponent } from "../../components/game/card_game/card_game.component";
 
 @Component({
   selector: 'home-page',
-  imports: [MiniPorfolioComponent, GreetingsComponent, SummaryComponent],
+  imports: [MiniPorfolioComponent, GreetingsComponent, SummaryComponent, CardGameComponent],
   templateUrl: './home-page.component.html'
 })
 
